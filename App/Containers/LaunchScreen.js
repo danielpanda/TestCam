6e999a75ec1bdc7e15713aeac1aa461339ef86bd
Daniel P. Simorangkir <asaneharuto@gmail.com>
2021-12-09 19:42:22 +0700

    update orientation & build apk

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -2,11 +2,21 @@
 import React, { PureComponent } from 'react';
 import { AppRegistry, StyleSheet, Text, TouchableOpacity, View, StatusBar } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import IdleTimerManager from 'react-native-idle-timer';
+
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends PureComponent {
+  componentWillMount() {
+    IdleTimerManager.setIdleTimerDisabled(true);
+  }
+  
+  componentWillUnmount() {
+    IdleTimerManager.setIdleTimerDisabled(false);
+  }
+  
   render() {
     return (
       <View style={styles.container}>
@@ -31,7 +41,7 @@ export default class LaunchScreen extends PureComponent {
             buttonNegative: 'Cancel',
           }}
           onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
+            // console.log(barcodes);
           }}
         />
         {/* <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
@@ -47,7 +57,7 @@ export default class LaunchScreen extends PureComponent {
     if (this.camera) {
       const options = { quality: 0.5, base64: true };
       const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
+      // console.log(data.uri);
     }
   };
 }
